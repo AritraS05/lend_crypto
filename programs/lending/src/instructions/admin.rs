f2186aa89d033cd1100bf995f244e5afec1cc5e1
aritra sarkar <aritrasarkar00007@gmail.com>
2025-02-26 15:23:03 +0000

    mint account created

--- programs/lending/src/instructions/admin.rs
+++ programs/lending/src/instructions/admin.rs
@@ -1,8 +1,24 @@
 use anchor_lang::prelude::*;
 use anchor_spl::token_interface::Mint;
+
+use crate::state::Bank;  
 #[derive(Accounts)]
 pub struct InitBank<'info>{
     #[account(mut)]
     pub signer: Signer<'info>,
     pub mint: InterfaceAccount<'info, Mint>,
+
+    #[account(
+        init,
+        payer = signer,
+        space = 8 + Bank::INIT_SPACE,
+        seeds = [mint.key().as_ref()],
+        bump,
+    )]
+    pub bank: Account<'info, Bank>,
+
+    #[account(
+        init,
+        token::mint = mint,
+    )]
 }
