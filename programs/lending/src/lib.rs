cf8918c85e644f5c9ae6ad8ce56f8c1dbdda99b8
aritra sarkar <aritrasarkar00007@gmail.com>
2025-03-13 16:24:12 +0000

    deposit & collateral

--- programs/lending/src/lib.rs
+++ programs/lending/src/lib.rs
@@ -1,6 +1,7 @@
 use anchor_lang::prelude::*;
-mod instructions;
 use instructions::*;  
+
+mod instructions;
 mod state;
 
 declare_id!("ASfMy7gLvX9sCuehGtXGASznTsb2vPEFLt6UuVMZPtJX");
@@ -8,5 +9,10 @@ declare_id!("ASfMy7gLvX9sCuehGtXGASznTsb2vPEFLt6UuVMZPtJX");
 #[program]
 pub mod lending {
     use super::*;
-
+    pub fn init_bank(ctx: Context<InitBank>, liquidation_threshold: u64, max_ltv: u64) -> Result<()> {
+        process_init_bank(ctx, liquidation_threshold, max_ltv)
+    }
+    pub fn init_user(ctx: Context<InitUser>, usdc_address: Pubkey) -> Result<()> {
+        process_init_user(ctx, usdc_address)
+    }
 }
